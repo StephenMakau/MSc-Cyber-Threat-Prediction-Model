38f00ce2780dfa1bbb4788455d1dfd6e0eb84f05
StephenMakau <106533200+StephenMakau@users.noreply.github.com>
2026-07-29 14:18:51 +0300

    Update cyber_threat_model.py

--- cyber_threat_model.py
+++ cyber_threat_model.py
@@ -139,15 +139,21 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 
 xgb_model = XGBClassifier(
-
     n_estimators=200,
-
     learning_rate=0.05,
-
     max_depth=4,
-
     random_state=42
+)
 
+
+xgb_model.fit(
+    X_train,
+    y_train
+)
+
+
+xgb_prediction = xgb_model.predict(
+    X_test
 )
 xgb_prediction = xgb_model.predict(
     X_test
